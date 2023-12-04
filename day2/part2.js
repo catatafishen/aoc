acfac9db1c4cbd3624cb4c95cb9a647318da3a4f
Henrik Westergård <henrik@westergard.org>
2023-12-04 20:57:18 +0200

    Day 2 update

--- day2/part2.js
+++ day2/part2.js
@@ -1,15 +1,18 @@
+const toSum = (a, b) => (a ?? 0) + b;
+const toProduct = (a, b) => (a ?? 1) * b;
+const getKnownAmountsInstance = () => {
+    const values = {};
+    return {
+        getPower: () => Object.values(values).reduce(toProduct), setAmount: function (amount, color) {
+            values[color] = Math.max(values[color] ?? 0, amount);
+            return this;
+        }
+    };
+};
 const getPower = (row) => row.split(": ")[1].split("; ")
     .flatMap(set => set.split(", "))
     .map(colorAmount => colorAmount.split(" "))
-    .map(([amount, color]) => [parseInt(amount, 10), color])
-    .reduce((knownAmounts, [amount, color], index, array) => {
-        if (amount > knownAmounts[color]) {
-            knownAmounts[color] = amount;
-        }
-        if (index < array.length - 1) {
-            return knownAmounts;
-        }
-        return knownAmounts["red"] * knownAmounts["green"] * knownAmounts["blue"];
-    }, {"red": 0, "green": 0, "blue": 0});
+    .reduce((knownAmounts, [amount, color]) => knownAmounts.setAmount(parseInt(amount, 10), color), getKnownAmountsInstance())
+    .getPower();
 
-export default input => input.split("\n").map(getPower).reduce((a, b) => a + b, 0);
+export default input => input.split("\n").map(getPower).reduce(toSum);
