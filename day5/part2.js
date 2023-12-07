99fdfd9a3fee1c2d5ee57210aae46644b85b7fe3
Henrik Westergård <henrik@westergard.org>
2023-12-07 11:34:51 +0200

    Reorder if statements so we usually can return early

--- day5/part2.js
+++ day5/part2.js
@@ -1,5 +1,5 @@
 const toMin = (a, b) => a < b ? a : b;
-const mapRanges = (rules, ranges) => {
+const mapRanges = (ranges, rules) => {
     let [mappedRanges, unmappedRanges] = rules.reduce(([mappedRanges, unmappedRanges], rule) => {
         const ruleInputStart = rule[1];
         const ruleLength = rule[2];
@@ -8,21 +8,27 @@ const mapRanges = (rules, ranges) => {
         unmappedRanges.forEach(range => {
             const rangeStart = range[0];
             const rangeLength = range[1];
-            if (rangeStart >= ruleInputStart && rangeStart <= ruleInputStart + ruleLength &&
-                rangeStart + rangeLength >= ruleInputStart && rangeStart + rangeLength <= ruleInputStart + ruleLength) { // Range within rule
+            if (rangeStart + rangeLength < ruleInputStart || rangeStart > ruleInputStart + ruleLength) {
+                newUnmappedRanges.push(range);
+                return;
+            }
+            const rangeStartsWithinRule = rangeStart >= ruleInputStart && rangeStart <= ruleInputStart + ruleLength;
+            const rangeEndsWithinRule = rangeStart + rangeLength >= ruleInputStart && rangeStart + rangeLength <= ruleInputStart + ruleLength;
+            if (rangeStartsWithinRule && rangeEndsWithinRule) {
                 mappedRanges.push([rangeStart + ruleOffset, rangeLength]);
-            } else if (rangeStart >= ruleInputStart && rangeStart <= ruleInputStart + ruleLength) { // Range starts within rule
-                newUnmappedRanges.push([ruleInputStart + ruleLength, (rangeStart + rangeLength) - (ruleInputStart + ruleLength)]);
-                mappedRanges.push([rangeStart + ruleOffset, ruleInputStart + ruleLength - rangeStart]);
-            } else if (rangeStart + rangeLength >= ruleInputStart && rangeStart + rangeLength <= ruleInputStart + ruleLength) { // Range ends within rule
-                newUnmappedRanges.push([rangeStart, ruleInputStart - rangeStart]);
-                mappedRanges.push([ruleInputStart + ruleOffset, rangeLength - (ruleInputStart - rangeStart)]);
-            } else if (rangeStart < ruleInputStart && rangeStart + rangeLength > ruleInputStart + ruleLength) { // Range overlaps rule
+            } else if (rangeStartsWithinRule) {
+                const ruleEnd = ruleInputStart + ruleLength;
+                mappedRanges.push([rangeStart + ruleOffset, ruleEnd - rangeStart]);
+                newUnmappedRanges.push([ruleEnd, (rangeStart + rangeLength) - ruleEnd]);
+            } else if (rangeEndsWithinRule) {
+                const newUnmappedLength = ruleInputStart - rangeStart;
+                newUnmappedRanges.push([rangeStart, newUnmappedLength]);
+                mappedRanges.push([ruleInputStart + ruleOffset, rangeLength - newUnmappedLength]);
+            } else {
+                // Range overlaps rule
                 newUnmappedRanges.push([rangeStart, ruleInputStart - rangeStart]);
                 mappedRanges.push([ruleInputStart + ruleOffset, ruleLength]);
                 newUnmappedRanges.push([ruleInputStart + ruleLength, rangeLength - ruleLength - (ruleInputStart - rangeStart)]);
-            } else {
-                newUnmappedRanges.push([rangeStart, rangeLength]);
             }
         });
         return [mappedRanges, newUnmappedRanges];
@@ -37,7 +43,7 @@ export default (input) => {
         .map(seedRange => [parseInt(seedRange[0], 10), parseInt(seedRange[1], 10)]);
     return sections
         .map(section => section.split(":\n")[1].split("\n").map(rule => rule.split(" ").map(s => parseInt(s, 10))))
-        .reduce((ranges, rules) => mapRanges(rules, ranges), seedRanges)
+        .reduce(mapRanges, seedRanges)
         .map(result => result[0])
         .reduce(toMin);
 }
